9139e7e43604abe28c5f9c338f54625b94a21ea9
aznjp <jpark103193@gmail.com>
2020-10-03 08:38:16 -0400

    added in array of questions that can be used by inquirer module for inputs

--- index.js
+++ index.js
@@ -3,8 +3,57 @@ const fs = require('fs')
 const util = require('util')
 
 // array of questions for user
-const questions = [
-
+const questions = [{
+        type: 'input',
+        message: 'What is the title of the Repository/Project?',
+        name: 'Title'
+    },
+    {
+        type: 'input',
+        message: 'Denote a description for what your project is.',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'What are the required files necessary to install for this project?',
+        name: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'Explain how the application can be used.',
+        name: 'Usage'
+    },
+    {
+        type: 'checkbox',
+        message: 'What are the license(s) placed on this project?',
+        name: 'License',
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'Academic Free License v3.0',
+            'Creative Commons license family',
+            'Education Community License v2.0',
+            'Do What the F*ck You Want to Public License'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'Contributers',
+        default: 'John Park'
+    },
+    {
+        type: 'input',
+        message: 'What is your Github UserName?',
+        name: 'Github',
+        default: 'aznjp'
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'Email',
+        default: 'Jpark103193@gmail.com'
+    },
 ];
 
 // function to write README file
