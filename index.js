b64e860d749cd26192db0b99ad473f63b835e242
aznjp <jpark103193@gmail.com>
2020-10-03 08:09:59 -0400

    added in modules that will be required for the utilization of this project

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const util = require('util')
+
 // array of questions for user
 const questions = [
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Congratulations, your README.md file is now ready for your application!")
+    });
+}
 
 // function to initialize program
 function init() {
